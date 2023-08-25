693f726da755f1ba8dee6bd275b73938439cd9e7
Luiz Moraes <luiz.moraes@pensiondynamics.com.br>
2023-08-25 10:49:13 -0300

    feat: add union and intersect methods

--- App.java
+++ App.java
@@ -9,5 +9,15 @@ public class App {
         list.add(2);
         System.out.println(Func.nOcorrencias(list, 2));
         System.out.println(Func.hasRepeat(list));
+
+        ArrayList<Integer> list2 = new ArrayList<Integer>();
+        list2.add(2);
+        list2.add(2);
+        list2.add(2);
+        list2.add(2);
+        list2.add(3);
+        list2.add(4);
+        System.out.println(Func.union(list, list2));
+        System.out.println(Func.intersect(list, list2));
     }
 }
--- Func.java
+++ Func.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Stream.concat;
+
 public class Func {
 
     public static int nOcorrencias(ArrayList<Integer> l, Integer el){
@@ -18,6 +21,16 @@ public class Func {
             }
         }
         return false;
+    }
+
+    public static ArrayList<Integer> union(ArrayList<Integer> l1, ArrayList<Integer> l2) {
+        // if no need to remove duplicates, just remove .distinct()
+        return new ArrayList<>(concat(l1.stream(), l2.stream()).distinct().collect(toList()));
+    }
+
+    public static ArrayList<Integer> intersect(ArrayList<Integer> l1, ArrayList<Integer> l2) {
+        // if no need to remove duplicates, just remove .distinct()
+        return new ArrayList<>(l1.stream().distinct().filter(l2::contains).collect(toList()));
     }
 
 }
